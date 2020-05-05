ee9057c1a5c44f54e278f1a67a0db4261228c3a2
jaydev sharma <43092234+jay-dev23@users.noreply.github.com>
2020-05-05 17:56:50 +0530

    Update App.js

--- instructorClient-master/src/App.js
+++ instructorClient-master/src/App.js
@@ -11,7 +11,7 @@ import {
 import { setContext } from 'apollo-link-context';
 import Layout from './components/layout';
 
-const uri = process.env.API_URI || 'http://localhost:4000/api';
+const uri = process.env.API_URI || 'http://18.220.4.122:4000/api';
 const httpLink = createHttpLink({ uri });
 const cache = new InMemoryCache();
 
